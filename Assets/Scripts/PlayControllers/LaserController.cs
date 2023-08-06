065c820ea69d14e705be3a0d1557675d015628e2
Justin Roderman <jcroderman@gmail.com>
2023-08-06 10:37:00 -0700

    Adds in laser to fire

--- Assets/Scripts/PlayControllers/LaserController.cs
+++ Assets/Scripts/PlayControllers/LaserController.cs
@@ -7,15 +7,26 @@ public class LaserController : MonoBehaviour
     private Animator animator;
     private PlayerController playerController;
 
+    private bool imFiringMahLazer;
+
     protected void Start()
     {
         animator = GetComponent<Animator>();
         playerController = transform.parent.GetComponent<PlayerController>();
+        imFiringMahLazer = false;
     }
 
     protected void Update()
     {
+        CheckForLaserBlast();
+
         animator.SetFloat("PlayerVelXAbs", Mathf.Abs(playerController.PlayerVelX));
         animator.SetFloat("PlayerVelY", playerController.PlayerVelY);
+        animator.SetBool("LaserBlast", imFiringMahLazer);
+    }
+
+    private void CheckForLaserBlast()
+    {
+        imFiringMahLazer = Input.GetKey(KeyCode.Space);
     }
 }
