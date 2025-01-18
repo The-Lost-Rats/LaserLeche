f05cd36729bb4a67d423d44e9d44015c42184803
Varun Chadha <vichadha@protonmail.com>
2025-01-18 08:57:44 -0800

    Comments and refactor health pickup

--- Assets/Scripts/PlayControllers/HealthPickupController.cs
+++ Assets/Scripts/PlayControllers/HealthPickupController.cs
@@ -3,21 +3,42 @@ using UnityEngine;
 // DoTween
 // using DG.Tweening;
 
+// Ideally there is a better way to share the action of this pickup
+// (either hardcoded or dynamic)
+
+/** Health pickup controller script
+ *
+ * Manages pickup lifecycle and collisions
+ */
 public class HealthPickupController : ParallaxObject
 {
+    /** Pickup related properties
+     */
     [Header("Pickup Properties")]
+
+    // Amount of health to give to player
     [SerializeField]
     private int healthToRestore;
 
+    /** Properties related to animation of pickup
+     */
     [Header("Animation Properties")]
+
+    // Speed of animation
     [SerializeField]
     [Range(0.01f, 5.0f)]
     private float speed;
 
 
-    // Private internal vars
+    /** Private internal vars
+     */
+
+    // If pickup has been picked up
     private bool isUsed;
 
+
+    /** Initialize values and animation
+     */
     protected override void OnStart()
     {
         // Set isUsed to false (we haven't picked this guy up yet!)
@@ -26,37 +47,43 @@ public class HealthPickupController : ParallaxObject
         // TODO: trigger DoTween Animation
     }
 
+    // TODO: delete this when dotween comes in
     protected void Update()
     {
-        // float y = Mathf.PingPong(Time.time * speed, 1) * 6 - 3;
-        // transform.position = new Vector3(0, y, 0);
+        float y = Mathf.PingPong(Time.time * speed, 1) * 2 - 6;
+        transform.position = new Vector2(transform.position.x, y);
     }
 
-    // Ideally there is a better way to share the action of this pickup
-    // (either hardcoded or dynamic)
-    public int getHealth()
+    /** Destroy self and any other clean up
+     */
+    public void Die()
     {
-        return healthToRestore;
-    }
-
-    public void die()
-    {
-        // Picked up and can be removed from scene
+        // I have been picked up and can be removed from scene
         Destroy(gameObject);
     }
 
+    /** Collision has occurred
+     */
     protected void OnTriggerEnter2D(Collider2D other)
     {
         // Only care about colliding with objects IF we haven't used the
         // pickup yet
-        if (isUsed)
+        if (!isUsed)
         {
-            return;
+            CheckCollision(other);
         }
+    }
 
+    /** Check collision
+     *
+     * If against player, use pick up
+     */
+    private void CheckCollision(Collider2D other)
+    {
+        // If player - update health and die
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("PLAYER");
+            Debug.Log("Player picked up health pickup!");
 
             // Set isUsed to true
             isUsed = true;
@@ -65,7 +92,8 @@ public class HealthPickupController : ParallaxObject
             other.gameObject.GetComponent<PlayerController>().UpdateHealth(healthToRestore);
 
             // Destroy self
-            die();
+            Die();
         }
+
     }
 }
